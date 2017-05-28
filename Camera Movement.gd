e7e0e26ebcb42081b5a3897061ffa34fdbf80d6f
Jonas Yamazaki <outopos@gmail.com>
2017-05-29 01:06:51 +0200

    Update

--- Camera Movement.gd
+++ Camera Movement.gd
@@ -1,14 +1,31 @@
 extends Spatial
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+export var vertical_lock = false
+export var pan_on_mouse_move = true
+
+onready var center = get_viewport().get_visible_rect().size / 2
+onready var pivot = get_node("Pivot")
+
+var rotation_deg = Vector3(0, 0, 0)
+var rotation_deg_offset = Vector3(0, 0, 0)
+
+var pressed = false
 
 func _ready():
+	rotation_deg = get_rotation_deg()
 	set_process_input(true)
 	
 func _input(event):
 	if event.type == InputEvent.MOUSE_MOTION:
-		var offset = (event.pos - center) / center
-		set_rotation_deg(Vector3(50 * offset.y, 0, 50 * - offset.x))
-		
+		if pressed and false:
+			rotation_deg += Vector3(0.1 * event.relative_y, 0 , 0.1 * -event.relative_x)
+			print(event.relative_pos)
+		if pan_on_mouse_move:
+			var offset = (event.pos - center) / center
+			rotation_deg_offset = Vector3(50 * offset.y, 0, 50 * - offset.x)
+		pivot.set_rotation_deg(rotation_deg + rotation_deg_offset)
+	
+	if event.type == InputEvent.MOUSE_BUTTON:
+		pressed = event.pressed
+		#print("poop")
+		#print(event.button_index, )
--- Game.gd
+++ Game.gd
@@ -1,11 +1,8 @@
 extends Spatial
 
-onready var center = get_viewport().get_visible_rect().size / 2
-onready var pivot = get_node("Pivot")
 
 func _ready():
 	set_process(true)
-	set_process_input(true)
 	
 	var shuffled_tiles = get_node("/root/Tiles").get_shuffled_tiles(1337)
 	print(shuffled_tiles)
@@ -14,12 +11,3 @@ func _ready():
 		var value = shuffled_tiles[i]
 		child.set_value(value)
 		i += 1
-	
-
-func _process(delta):
-	pass
-	
-func _input(event):
-	if event.type == InputEvent.MOUSE_MOTION:
-		var offset = (event.pos - center) / center
-		pivot.set_rotation_deg(Vector3(50 * offset.y, 0, 50 * - offset.x))
