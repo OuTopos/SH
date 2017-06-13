67aa6816133b473e3411b66f450c94370c23571b
Jonas Yamazaki <outopos@gmail.com>
2017-06-13 08:03:22 +0200

    Update

--- Game.gd
+++ Game.gd
@@ -4,13 +4,41 @@ onready var tile_size  = get_node("/root/Tiles").tile_size
 
 
 var moves = Array()
-var selected = null
+var selected_tile = null
+var counter_click = 0
+var counter_move = 0
+var counter_undo = 0
+var counter_time = 0
+
+var ticker_second = 0
 
 var tile_scene = preload("res://Tile.tscn")
 onready var layout_node = get_node("Layout")
 
 func _ready():
 	set_process(true)
+
+func _process(delta):
+	counter_time += delta
+	ticker_second += delta
+	
+	if ticker_second > 1:
+		ticker_second -= 1
+		update_labels()
+			
+
+func update_labels():
+	
+	var seconds = int(counter_time) % 60
+	var minutes = int(counter_time) % 3600 / 60
+	var time_formatted = "%02d:%02d" % [minutes, seconds]
+	get_node("PanelContainer/VBoxContainer/HBoxContainer/Counter Time").set_text("Time: " + time_formatted)
+	get_node("PanelContainer/VBoxContainer/HBoxContainer/Counter Click").set_text("Clicks: " + str(counter_click))
+	get_node("PanelContainer/VBoxContainer/HBoxContainer/Counter Move").set_text("Moves: " + str(counter_move))
+	get_node("PanelContainer/VBoxContainer/HBoxContainer/Counter Undo").set_text("Undos: " + str(counter_undo))
+	
+	
+	
 	
 
 func load_layout(layout_name):
@@ -32,42 +60,65 @@ func load_layout(layout_name):
 	for tile in layout["tiles"]:
 		
 		var tile_node = tile_scene.instance()
-		tile_node.set_translation(Vector3(tile.x * tile_size.x, tile.y * tile_size.y, tile.z * tile_size.z))
+		#tile_node.set_translation(Vector3(tile.x * tile_size.x, tile.y * tile_size.y, tile.z * tile_size.z))
 		tile_node.connect("clicked", self, "_on_Tile_clicked")
 		layout_node.add_child(tile_node)
+		tile_node.place(Vector3(tile.x * tile_size.x, tile.y * tile_size.y, tile.z * tile_size.z))
 		
 		
-		tile_node.hide()
-		tile_node.delay = i * 0.03
-		#tile_node.place(i * 0.05)
+		#tile_node.hide()
+		tile_node.delay = i * 0.02
 		i += 1
 		
 		
 	var placed_tiles = layout_node.get_child_count()
 	print(str(placed_tiles) + " tiles loaded.")
+
+func select_tile(tile):
+	tile.set_selected(true)
+	selected_tile = tile
+
+func deselect_tile():
+	if selected_tile:
+		selected_tile.set_selected(false)
+		selected_tile = null
 	
-func _on_Tile_clicked(tile):
-	print("yes clicked")
-	if tile.blocked:
-		print("BLOCKED")
-	elif not tile.selected:
-		if selected:
-			if tile.value == selected.value:
-				tile.queue_free()
-				selected.queue_free()
-				selected = null
-				
-				get_tree().call_group(0, "Tiles", "update_blocked")
-			else:
-				selected.selected = false
-				selected = null
+
+	
+func _on_Tile_clicked(clicked_tile):
+	counter_click += 1
+	if selected_tile:
+		# Already selected a tile.
+		if clicked_tile.blocked:
+			# Clicked on a blocked tile, deselect selected tile and play fail sound.
+			deselect_tile()
+			# FAIL BLEEP BLOOP
+			
+		elif clicked_tile.selected:
+			# Clicked on already selected tile, deselect it.
+			deselect_tile()
+		
+		elif clicked_tile.value == selected_tile.value:
+			counter_move += 1
+			var move = [selected_tile, clicked_tile]
+			moves.append(move)
+			# Found a match, remove both.
+			clicked_tile.disable()
+			selected_tile.disable()
+			selected_tile = null
+			# Update blocked status
+			get_tree().call_group(0, "Tiles", "update_blocked")
 		else:
-			tile.selected = true
-			selected = tile
-			print("SELECTING")
-	elif tile.selected:
-		print("DESELECTING")
-	print(tile)
+			# Clicked on a free but not matching tile, deselct selected tile.
+			deselect_tile()
+		
+	elif not clicked_tile.blocked:
+		# Clicked on a free tile, select it.
+		select_tile(clicked_tile)
+		
+	else:
+		# Clicked on blocked tile, play fail sound.
+		pass
 	
 func _on_Start_button_up():
 	load_layout("turtle")
@@ -79,3 +130,13 @@ func _on_Start_button_up():
 		var value = shuffled_tiles[i]
 		child.set_value(value)
 		i += 1
+
+
+func _on_Undo_button_up():
+	if moves.size() > 0:
+		counter_undo += 1
+		moves.back()[0].enable()
+		moves.back()[1].enable()
+		moves.remove(moves.size()-1)
+		deselect_tile()
+		get_tree().call_group(0, "Tiles", "update_blocked")
--- Tile.gd
+++ Tile.gd
@@ -9,12 +9,16 @@ var value = null
 
 # Member variables
 var gray_mat = FixedMaterial.new()
+var free_mat = FixedMaterial.new()
 var selected_mat = FixedMaterial.new()
-var selected = false
+
 var delay = 1
 var hidden = true
 var timer = 0
 
+var placed = false
+
+var selected = false
 var blocked = false
 
 var should_test = true
@@ -25,11 +29,16 @@ onready var visual_node = get_node("Visual")
 
 onready var global_tiles_node = get_node("/root/Tiles")
 
+var translation_offset = Vector3(0, 0, 0)
+var rotation_offset = Vector3(0, 0, 0)
+var scale_mod = Vector3(0.9, 0.9, 0.9)
+
 func _ready():
 	set_process(true)
 	set_fixed_process(true)
 	
 	
+	free_mat.set_parameter(FixedMaterial.PARAM_DIFFUSE, Color(0.4, 0.7, 0.4, 1))
 	selected_mat.set_parameter(FixedMaterial.PARAM_DIFFUSE, Color(0.16, 0.56, 0.91, 1))
 	
 	var draw = get_node("ImmediateGeometry")
@@ -51,9 +60,10 @@ func _ready():
 
 func _process(delta):
 	timer += delta
-	if hidden and timer >= delay:
-		place(0.4)
-		hidden = false
+	if not placed and timer >= delay:
+		visual_animate_place(0.4)
+		placed = true
+		
 func _fixed_process(delta):
 	if should_test:
 		set_blocked(raycast_blocked())
@@ -66,6 +76,7 @@ func _input_event(camera, event, pos, normal, shape):
 	if (event.type==InputEvent.MOUSE_BUTTON and event.pressed):
 		print("clicked")
 		emit_signal("clicked", self)
+		print(get_visual_rotation())
 		#if not selected and not blocked:
 		#	selected = true
 		#	print("selected")
@@ -82,24 +93,35 @@ func _input_event(camera, event, pos, normal, shape):
 
 
 func _mouse_enter():
-	get_node("Visual").set_scale(Vector3(1.1, 1.1, 1.1))
+	visual_animate_mouse_enter(0.1)
 
 
 func _mouse_exit():
-	get_node("Visual").set_scale(Vector3(1, 1, 1))
+	visual_animate_mouse_exit(0.2)
+
+func set_selected(b):
+	selected = b
+	if selected:
+		visual_node.get_node("MeshInstance").set_material_override(selected_mat)
+	else:
+		if blocked:
+			visual_node.get_node("MeshInstance").set_material_override(gray_mat)
+		else:
+			visual_node.get_node("MeshInstance").set_material_override(free_mat)
+	
 
 func set_blocked(b):
 	blocked = b
 	if blocked:
 		visual_node.get_node("MeshInstance").set_material_override(gray_mat)
 	else:
-		visual_node.get_node("MeshInstance").set_material_override(selected_mat)
+		visual_node.get_node("MeshInstance").set_material_override(free_mat)
 
 func raycast_blocked_side(side):
 	var space_state = get_world().get_direct_space_state()
 	
 	for ray in global_tiles_node.rays[side]:
-		var result = space_state.intersect_ray( get_translation() + ray["from"], get_translation() + ray["to"], [self])
+		var result = space_state.intersect_ray( get_translation() + ray["from"], get_translation() + ray["to"], [self], 1)
 		if not result.empty():
 			return true
 	return false
@@ -117,9 +139,69 @@ func set_value(v):
 	var texture = get_node("/root/Tiles").textures[v]
 	get_node("Visual/Sprite3D").set_texture(texture)
 
-func place(time):
-	tween_node.interpolate_property(visual_node, "transform/translation", Vector3(0, 25, -25), Vector3(0, 0.5, 0), time, Tween.TRANS_SINE, Tween.EASE_OUT)
+	
+func get_visual_translation():
+	var x = randf()*0.1-0.05
+	var y = 0
+	var z = randf()*0.1-0.05
+	return Vector3(x, y, z)
+
+func get_visual_rotation():
+	var x = randf()*0.5-0.25
+	var y = randf()*2-1
+	var z = randf()*0.5-0.25
+	return Vector3(x, y, z)
+
+func get_visual_scale():
+	var x = 0.97
+	var y = 0.97
+	var z = 0.97
+	return Vector3(x, y, z)
+	
+func place(pos):
+	set_translation(pos)
+	visual_node.set_translation(Vector3(0, 25, 0))
+	
+	
+	#visual_node.set_translation(get_visual_translation())
+	#visual_node.set_rotation_deg(get_visual_rotation())
+	visual_node.set_scale(get_visual_scale())
+
+
+	
+func visual_animate_mouse_enter(time):
+	tween_node.interpolate_property(visual_node, "transform/translation", visual_node.get_translation(), visual_node.get_translation() + Vector3(0, 1, 0), time, Tween.TRANS_SINE, Tween.EASE_OUT)
 	tween_node.start()
-	tween_node.interpolate_property(visual_node, "transform/rotation", Vector3(0, 90, -25), Vector3(0, 0, 0), time, Tween.TRANS_SINE, Tween.EASE_OUT)
+	
+	tween_node.interpolate_property(visual_node, "transform/rotation", visual_node.get_rotation(), Vector3(0, 0, 0), time, Tween.TRANS_SINE, Tween.EASE_OUT)
 	tween_node.start()
-	show()
+
+func visual_animate_mouse_exit(time):
+	tween_node.interpolate_property(visual_node, "transform/translation", visual_node.get_translation(), get_visual_translation(), time, Tween.TRANS_BOUNCE, Tween.EASE_OUT)
+	tween_node.start()
+	
+	tween_node.interpolate_property(visual_node, "transform/rotation", visual_node.get_rotation(), get_visual_rotation(), time, Tween.TRANS_SINE, Tween.EASE_OUT)
+	tween_node.start()
+	
+func visual_animate_place(time):
+	tween_node.interpolate_property(visual_node, "transform/translation", visual_node.get_translation(), get_visual_translation(), time, Tween.TRANS_SINE, Tween.EASE_OUT)
+	tween_node.start()
+	
+	tween_node.interpolate_property(visual_node, "transform/rotation", visual_node.get_rotation(), get_visual_rotation(), time, Tween.TRANS_SINE, Tween.EASE_OUT)
+	tween_node.start()
+	
+func visual_animate_remove(time):
+	tween_node.interpolate_property(visual_node, "transform/translation", visual_node.get_translation(), get_visual_translation() + Vector3(0, 25, 0), time, Tween.TRANS_SINE, Tween.EASE_OUT)
+	tween_node.start()
+	
+	tween_node.interpolate_property(visual_node, "transform/rotation", visual_node.get_rotation(), get_visual_rotation(), time, Tween.TRANS_SINE, Tween.EASE_OUT)
+	tween_node.start()
+
+func enable():
+	visual_animate_place(0.4)
+	set_layer_mask(1)
+	
+func disable():
+	visual_animate_remove(0.4)
+	set_layer_mask(0)
+	set_selected(false)
